2767f5c959def572e6be51b2dc7394481c70da9f
Alex <Koblosh.Alex@gmail.com>
2020-11-24 16:34:00 -0500

    progress

--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
-const express = require("express");
 const path = require("path");
+const http = require("http");
+const express = require("express");
+const socketio = require("socket.io");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const server = http.createServer(app);
+const io = socketio(server);
 
+const port = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicDirectoryPath));
 
-app.listen(port, () => {
-	console.log(`Server is running on ${port}`);
+io.on("connection", (socket) => {
+	console.log("New WebSocket connection");
+
+	socket.emit("message", "Welcome");
+	socket.broadcast.emit("message", "new user have joined");
+
+	socket.on("sendMessage", (message) => {
+		io.emit("message", message);
+	});
+
+	socket.on("disconnect", () => {
+        io.emit("message", "A user has left the chat")
+    });
+});
+
+server.listen(port, () => {
+	console.log(`Server is up on port ${port}!`);
 });
